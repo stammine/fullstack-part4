ff364d3ef83ab6864371a3bcce5a81d4d90caea7
stammine <samutamm@yandex.com>
2020-07-25 16:18:33 +0300

    exercises 4.15 - 4.19

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,33 +1,58 @@
+const jwt = require('jsonwebtoken')
+
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
+const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
   const params = request.body
+
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   params.likes = params.likes || 0
   if (params.title === undefined && params.url === undefined) {
-    response.status(400).json('title or url required')
+    response.status(400).json({ error: 'title or url required' })
     return
   }
+  params.user = user._id
+
   const blog = new Blog(params)
-  const result = await blog.save()
-  response.status(201).json(result)
+  const savedBlog = await blog.save()
+
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
+  response.status(201).json(savedBlog)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
-  response.status(201).json('deleted')
+  response.status(201).json({ message: 'deleted' })
 })
 
 blogsRouter.put('/:id', async (request, response) => {
   const params = request.body
 
   if (params.title === undefined && params.url === undefined) {
-    response.status(400).json('title or url required')
+    response.status(400).json({ error: 'title or url required' })
     return
   }
 
